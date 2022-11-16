bc2afe28df0409e54809b0ce7a16b73759b510f8
jooman21 <jooman2121@gmail.com>
2022-11-16 09:26:43 +0300

    jooman21

---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+products:[
+    {
+        name: 'Nike Slim shirt',
+        slug: 'Nike-Slim-shirt',
+        category: 'Shirts',
+        image: 'images/p1.jpg',
+        price: '120',
+        countInStock: 10,
+        brand: 'Nike',
+        rating: '4.5',
+        numReviews: '10',
+        description: 'High Quality Shirt'
+
+    },
+    {
+        name: 'Adidas fit SHIRT',
+        slug: 'Adidas fit-Shirt',
+        category: 'Shirts',
+        image: 'images/p2.jpg',
+        price: '250',
+        countInStock: 20,
+        brand: 'Adidas',
+        rating: '4.0',
+        numReviews: '10',
+        description: 'High Quality Shirt'
+
+    },
+    {
+        name: 'Nike Slim Pant',
+        slug: 'Nike-Slim-Pant',
+        category: 'Pants',
+        image: 'images/p3.jpg',
+        price: '20',
+        countInStock: 10,
+        brand: 'Nike',
+        rating: '4.5',
+        numReviews: '14',
+        description: 'High Quality Product'
+
+    },
+    {
+        name: 'Adidas fit pant',
+        slug: 'Adidas-fit-pant',
+        category: 'Pants',
+        image: 'images/p4.jpg',
+        price: '30',
+        countInStock: 10,
+        brand: 'Nike',
+        rating: '4.5',
+        numReviews: '18',
+        description: 'High Quality Product'
+
+    },
+],
+
+}
+export default data;
